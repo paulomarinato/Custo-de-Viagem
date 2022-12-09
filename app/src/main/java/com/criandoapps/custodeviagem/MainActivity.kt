9f13d5778b0a0ce9e01100633073040377a0633e
paulomarinato <marinatopaulo@gmail.com>
2022-12-09 00:03:19 -0300

    Configuração ViewBinding concluida

--- app/src/main/java/com/criandoapps/custodeviagem/MainActivity.kt
+++ app/src/main/java/com/criandoapps/custodeviagem/MainActivity.kt
@@ -2,10 +2,26 @@ package com.criandoapps.custodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.criandoapps.custodeviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.butonCalculete.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.buton_calculete) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        Toast.makeText(this, "Fui Clicado", Toast.LENGTH_SHORT).show()
     }
 }
